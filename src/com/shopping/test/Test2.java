36c99a8371d2ae5293a2846c567335effc8a4b0a
zhangqi <846377079@qq.com>
2019-07-01 20:34:29 +0800

    删除和新增文件Test2文件

---
+++ src/com/shopping/test/Test2.java
@@ -0,0 +1,10 @@
+package com.shopping.test;
+
+/**
+ * Created by Administrator on 2019/7/1 0001.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("==================测试成功！============");
+    }
+}
