f70da10debcb8f0abf29f2539c45913b94b2f82d
开到荼蘼 <846377079@qq.com>
2019-07-01 21:30:07 +0800

    Update Test2.java

--- src/com/shopping/test/Test2.java
+++ src/com/shopping/test/Test2.java
@@ -8,6 +8,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("==================测试成功！============");
         System.out.println("==================测试成功1！============");
-        System.out.println("==================测试成功5！============");
+        System.out.println("==================测试成功8！============");
     }
 }
